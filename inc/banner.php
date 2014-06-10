0a19221eebe91eb7d893f4002d2868074d9a90b1
Peter Grochowski <piogrek@gmail.com>
2014-06-10 23:06:01 +0100

    initial

---
+++ inc/banner.php
@@ -0,0 +1,1 @@
+<img src="http://placehold.it/2048x200&text=banner">
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * main template file
+ */
+?><!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8" />
+
+        <link rel='stylesheet' id='bootstrap-css'  href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css?ver=7b10204' type='text/css' media='all' />
+        <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js?ver=7b10204'></script>
+    </head>
+
+    <body>
+
+        <?php include 'inc/logo.php'; ?>
+
+        <?php include 'inc/navigation.php'; ?>
+
+        <?php include 'inc/banner.php'; ?>
+
+        <?php include 'inc/content.php'; ?>
+
+        <script type='text/javascript' src='//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js?ver=7b10204'></script>
+    </body>
+</html>
