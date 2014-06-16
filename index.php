1367c407fa6fb42763ab7b0e9451d4cf0f96b97f
Accolade Web Pvt. Ltd. <webaccolade@gmail.com>
2014-06-16 20:45:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,402 @@
-<?php
-/**
- * main template file
- */
-?><!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <meta charset="UTF-8" />
+<!doctype html>
 
-        <link rel='stylesheet' id='bootstrap-css'  href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css?ver=7b10204' type='text/css' media='all' />
-        <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js?ver=7b10204'></script>
-    </head>
 
-    <body>
+<head>
+	<title>This is a demo page</title>
+	
+	<meta charset="utf-8">
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+    
+       
+    <!-- this styles only adds some repairs on idevices  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Google fonts - witch you want to use - (rest you can just remove) -->
+   	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+    
+    <!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+    
+    <!-- ######### CSS STYLES ######### -->
+	
+    <link rel="stylesheet" href="css/reset.css" type="text/css" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+    
+    <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
+    
+    <!-- responsive devices styles -->
+	<link rel="stylesheet" media="screen" href="css/responsive-leyouts.css" type="text/css" />
+    
+    <!-- mega menu -->
+    <link href="js/mainmenu/sticky.css" rel="stylesheet">
+    <link href="js/mainmenu/bootstrap.css" rel="stylesheet">
+    <link href="js/mainmenu/fhmm.css" rel="stylesheet">
+    
+    <!-- REVOLUTION SLIDER -->
+   	<link rel="stylesheet" type="text/css" href="js/revolutionslider/rs-plugin/css/settings.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="js/revolutionslider/css/slider_main.css" media="screen" />
+    
+    <!-- cubeportfolio -->
+	<link rel="stylesheet" type="text/css" href="js/cubeportfolio/cubeportfolio.min.css">
+    
+	<!-- tabs -->
+    <link rel="stylesheet" type="text/css" href="js/tabs/assets/css/responsive-tabs.css">
+    <link rel="stylesheet" type="text/css" href="js/tabs/assets/css/responsive-tabs2.css">
 
-        <?php include 'inc/logo.php'; ?>
+	<!-- carousel -->
+    <link rel="stylesheet" href="js/carousel/flexslider.css" type="text/css" media="screen" />
+ 	<link rel="stylesheet" type="text/css" href="js/carousel/skin.css" />
+    
+    <!-- accordion -->
+    <link rel="stylesheet" href="js/accordion/accordion.css" type="text/css" media="all">
+       
+ 
+    <!-- Lightbox -->
+    <link rel="stylesheet" type="text/css" href="js/lightbox/jquery.fancybox.css" media="screen" />
+       
+ 
+</head>
 
-        <?php include 'inc/navigation.php'; ?>
+<body>
 
-        <?php include 'inc/banner.php'; ?>
 
-        <?php include 'inc/content.php'; ?>
+<div class="site_wrapper">
 
-        <script type='text/javascript' src='//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js?ver=7b10204'></script>
-    </body>
+<header id="header">
+
+	<!-- Top header bar -->
+	<div id="topHeader">
+    
+	<div class="wrapper">
+         
+        <div class="top_nav">
+        <div class="container">
+        
+        <div class="left">
+            
+            <ul>
+                <li><a href="mailto:info@yourdomain.com"><i class="fa fa-envelope"></i> info@yourdomain.com</a></li>
+                <li><i class="fa fa-phone-square"></i> +91 982 738 8520</li>
+            </ul>
+            
+        </div><!-- end left links -->
+        
+        <div class="right">
+            
+            <ul>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa fa-flickr"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+            </ul>
+            
+        </div><!-- end right social links -->
+        
+        </div>
+        </div>
+            
+ 	</div>
+    
+	</div><!-- end top navigation -->
+	
+    
+	<div id="trueHeader">
+    
+	<div class="wrapper">
+    
+     <div class="container">
+    
+		<!-- Logo -->
+		<div class="logo"><a href="index.html" id="logo"></a></div>
+		
+        <!-- Menu -->
+        <div class="menu_main">
+        
+          <nav class="navbar navbar-default fhmm" role="navigation">
+             <div class="navbar-header">
+          		<button type="button" data-toggle="collapse" data-target="#defaultmenu" class="navbar-toggle">Navigation <i class="fa fa-bars tbars"></i></button>
+			 </div><!-- end navbar-header -->
+        
+            <div id="defaultmenu" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="index.html">About Me</a></li>
+                    <li><a href="index.html">Contact</a></li>
+                    
+                </ul><!-- end nav navbar-nav -->
+          
+              
+          
+                
+		</div><!-- end #navbar-collapse-1 -->
+            
+		</nav>
+            
+		</div>
+        
+        </div>
+		
+	</div>
+    
+	</div>
+    
+</header>
+
+<div class="clearfix"></div>
+
+<!-- Slider
+======================================= -->  
+
+<div class="container_full">
+    
+    <div class="tp-banner-container">
+		<div class="tp-banner" >
+        
+			<ul>
+            	<!-- SLIDE 1 -->
+				<li data-transition="fade" data-slotamount="7" data-masterspeed="1500" >
+                
+					<!-- MAIN IMAGE -->
+					<img src="http://placehold.it/1920x610" alt=""  data-bgfit="cover" data-bgposition="center top" data-bgrepeat="no-repeat">
+                       
+                    <div class="tp-caption bigtext sfb"
+						data-x="center"
+						data-hoffset="0"
+						data-voffset="-10"
+						data-y="200"
+						data-speed="800"
+						data-start="1000"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="off"
+						style="z-index: 6">Sample  <strong>Slider</strong>
+					</div>
+                    
+                    <div class="tp-caption smalltext sfb"
+						data-x="center"
+						data-hoffset="0"
+						data-voffset="-10"
+						data-y="318"
+						data-speed="800"
+						data-start="1600"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="off"
+						style="z-index: 6">Powerfully Creativity with Professional Clean Developed Slider <br /> We Build Smarter Stuff - Let’s Start an Awesome Gallery!
+					</div>
+                    
+                </li>
+                
+                <!-- SLIDE 2 -->
+				<li data-transition="zoomout" data-slotamount="7" data-masterspeed="1500" >
+                
+					<!-- MAIN IMAGE -->
+					<img src="http://placehold.it/1920x610" alt=""  data-bgfit="cover" data-bgposition="center top" data-bgrepeat="no-repeat">
+                       
+  					
+					
+                    <div class="tp-caption circle1 sft"
+						data-x="12"
+						data-y="243"
+						data-speed="800"
+						data-start="1000"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="off"
+						style="z-index: 6"><i class="fa fa-cog fa-3x"></i>
+					</div>
+                    
+                    
+                    <div class="tp-caption circle1 sft"
+						data-x="157"
+						data-y="243"
+						data-speed="800"
+						data-start="1300"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="off"
+						style="z-index: 6"><i class="fa fa-bug fa-3x"></i>
+					</div>
+                    
+                    
+                    <div class="tp-caption circle1 sft"
+						data-x="304"
+						data-y="243"
+						data-speed="800"
+						data-start="1600"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="off"
+						style="z-index: 6"><i class="fa fa-comments fa-3x"></i>
+					</div>
+                    
+                    <div class="tp-caption circle1 sft"
+						data-x="449"
+						data-y="243"
+						data-speed="800"
+						data-start="1900"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="off"
+						style="z-index: 6"><i class="fa fa-coffee fa-3x"></i>
+					</div>
+                    
+                    
+                    
+                    <div class="tp-caption small_divider skewfromleft customout"
+						data-x="647"
+						data-y="247"
+						data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+						data-speed="800"
+						data-start="2200"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="on"
+						style="z-index: 9">
+					</div>
+                    
+                    
+                    
+                    <div class="tp-caption bigtext_bold skewfromleft customout"
+						data-x="693"
+						data-y="212"
+						data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+						data-speed="800"
+						data-start="2500"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="on"
+						style="z-index: 9">most flexible &amp;
+					</div>
+                    
+                    
+                    
+                    <div class="tp-caption bigtext_bold02 skewfromright customout"
+						data-x="691"
+						data-y="260"
+						data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+						data-speed="800"
+						data-start="2800"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="on"
+						style="z-index: 9">many unique
+					</div>
+					
+                    
+                    
+                     <div class="tp-caption bigtext_extrathin skewfromleft customout"
+						data-x="691"
+						data-y="298"
+						data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+						data-speed="800"
+						data-start="3100"
+						data-easing="Power4.easeOut"
+						data-endspeed="300"
+						data-endeasing="Power1.easeIn"
+						data-captionhidden="on"
+						style="z-index: 9">features
+					</div>
+				</li>
+            </ul>
+
+		</div>
+	</div>
+
+
+</div><!-- end slider -->
+
+
+<div class="clearfix"></div>
+
+<div class="features_sec1">
+<div class="container">
+
+	<p>Pie chocolate bar pie. Icing apple pie halvah brownie powder. Gingerbread gummi bears tootsie roll chocolate cake chocolate cake ice cream sweet roll cake gummi bears. Oat cake bonbon jelly beans chocolate bar marshmallow fruitcake cotton candy chocolate bar croissant. Unerdwear.com croissant bonbon chocolate bar biscuit marzipan sugar plum gummies. Powder toffee sesame snaps topping jujubes. Candy canes dessert toffee muffin. Croissant topping chocolate toffee donut macaroon cookie jelly beans. Biscuit danish bonbon lemon drops. Sweet croissant fruitcake lollipop oat cake. Topping sugar plum sesame snaps halvah. Candy tootsie roll liquorice sweet cake brownie bear claw liquorice chocolate cake. Pastry applicake apple pie muffin gingerbread chocolate bar chocolate bear claw. Jujubes sugar plum halvah sweet sweet roll.</p>
+
+<p>Oat cake gingerbread jujubes sweet roll macaroon lollipop soufflé. Gummies cookie candy canes bonbon lollipop sweet toffee cake liquorice. Tart biscuit tootsie roll. Cookie chupa chups soufflé fruitcake jelly sweet roll wafer. Marshmallow sugar plum caramels jelly beans unerdwear.com. Croissant croissant apple pie caramels powder jelly-o. Sesame snaps wafer gummi bears liquorice dessert dessert gummies wafer pudding. Oat cake cookie icing caramels chocolate bar toffee liquorice halvah. Chocolate cake powder macaroon applicake cake. Dessert liquorice jujubes cupcake chocolate cake applicake jelly caramels. Lollipop cake chupa chups. Unerdwear.com chocolate jelly-o macaroon oat cake sweet roll liquorice. Marzipan donut jujubes.</p>
+
+<p>Gingerbread fruitcake cupcake chocolate bar icing pastry. Sesame snaps sweet roll sugar plum lollipop bonbon soufflé. Cotton candy tart cheesecake. Icing caramels cupcake chocolate bar. Sweet marzipan cookie wafer donut croissant ice cream chocolate bar icing. Halvah lollipop caramels. Gummies applicake bear claw liquorice gummies icing pie sesame snaps. Gummies pie tart tiramisu. Fruitcake liquorice sesame snaps toffee chupa chups cheesecake. Cupcake cotton candy fruitcake. Soufflé ice cream powder bear claw cupcake gummies jelly-o cake. Candy cupcake ice cream croissant. Bonbon tootsie roll oat cake icing cotton candy caramels dragée liquorice.</p>
+
+</div>
+</div>    
+
+<!-- ######### JS FILES ######### -->
+<!-- get jQuery from the google apis -->
+<script type="text/javascript" src="js/universal/jquery.js"></script>
+
+
+
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
+<script type="text/javascript" src="js/revolutionslider/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+<script type="text/javascript" src="js/revolutionslider/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+<!-- mega menu -->
+<script src="js/mainmenu/bootstrap.min.js"></script>
+<script src="js/mainmenu/fhmm.js"></script>
+
+<!-- jquery jcarousel -->
+<script type="text/javascript" src="js/carousel/jquery.jcarousel.min.js"></script>
+
+<!-- scroll up -->
+<script src="js/scrolltotop/totop.js" type="text/javascript"></script>
+
+<!-- tabs -->
+<script src="js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script>
+
+<!-- jquery jcarousel -->
+<script type="text/javascript">
+	jQuery(document).ready(function() {
+			jQuery('#mycarouselthree').jcarousel();
+	});
+</script>
+
+<!-- accordion -->
+<script type="text/javascript" src="js/accordion/custom.js"></script>
+
+<!-- REVOLUTION SLIDER -->
+<script type="text/javascript" src="js/revolutionslider/rs-plugin/js/custom.js"></script>
+
+<!-- sticky menu -->
+<script type="text/javascript" src="js/mainmenu/sticky.js"></script>
+<script type="text/javascript" src="js/mainmenu/modernizr.custom.75180.js"></script>
+
+<!-- cubeportfolio -->
+<script type="text/javascript" src="js/cubeportfolio/jquery.cubeportfolio.min.js"></script>
+<script type="text/javascript" src="js/cubeportfolio/main.js"></script>
+
+<!-- carousel -->
+<script defer src="js/carousel/jquery.flexslider.js"></script>
+<script defer src="js/carousel/custom.js"></script>
+
+<!-- lightbox -->
+<script type="text/javascript" src="js/lightbox/jquery.fancybox.js"></script>
+<script type="text/javascript" src="js/lightbox/custom.js"></script>
+
+
+<script type="text/javascript">
+	// Menu drop down effect
+	$('.dropdown-toggle').dropdownHover().dropdown();
+	$(document).on('click', '.fhmm .dropdown-menu', function(e) {
+	  e.stopPropagation()
+	})
+</script>
+
+</body>
 </html>
